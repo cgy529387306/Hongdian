9fbce1c531ca517c02ddfacea5477100acb0f532
cgy529387306 <529387306@qq.com>
2018-12-02 22:47:20 +0800

    vvv

--- app/src/main/java/com/android/mb/hd/MainActivity.java
+++ app/src/main/java/com/android/mb/hd/MainActivity.java
@@ -1,13 +1,20 @@
 package com.android.mb.hd;
 
 import android.annotation.SuppressLint;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.KeyEvent;
+import android.webkit.GeolocationPermissions;
 import android.webkit.SslErrorHandler;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -26,6 +33,10 @@ import cn.sharesdk.wechat.friends.Wechat;
 public class MainActivity extends AppCompatActivity implements JavaScriptInterface.JsCallbackHandler{
     private WebView webView;
     private String webUrl = "http://47.106.132.144/hbapp/index.html";
+
+    public static final int FILE_CHOOSER_RESULT_CODE = 5173;
+    private ValueCallback<Uri> uploadMessage;
+    private ValueCallback<Uri[]> uploadMessageAboveL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,13 +110,15 @@ public class MainActivity extends AppCompatActivity implements JavaScriptInterfa
     private void initWebView(){
         webView = (WebView) findViewById(R.id.webView);
         webView.setWebViewClient(webViewClient);
-        webView.setWebChromeClient(new WebChromeClient());
+        setWebChromeClient();
         WebSettings webSettings = webView.getSettings();
         webSettings.setSavePassword(true);
         webSettings.setSaveFormData(true);
         webSettings.setJavaScriptEnabled(true);//允许使用js
         webSettings.setLoadWithOverviewMode(true);
         webSettings.setDomStorageEnabled(true);
+        webSettings.setSupportZoom(true);
+        webSettings.setGeolocationEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         Map<String,String> headerMap = new HashMap<>();
         headerMap.put("deviceType","1");
@@ -123,6 +136,66 @@ public class MainActivity extends AppCompatActivity implements JavaScriptInterfa
                 qqLoginComplete(jsonObject);
             }
         }
+    }
+
+    private void setWebChromeClient(){
+        webView.setWebChromeClient(new WebChromeClient(){
+            // For 3.0+ Devices (Start)
+            protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                uploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image");
+                startActivityForResult(Intent.createChooser(i, "File Browser"), FILE_CHOOSER_RESULT_CODE);
+            }
+            // For Lollipop 5.0+ Devices
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            public boolean onShowFileChooser(WebView mWebView,
+                                             ValueCallback<Uri[]> filePathCallback,
+                                             WebChromeClient.FileChooserParams fileChooserParams) {
+
+                if (uploadMessageAboveL != null) {
+                    uploadMessageAboveL.onReceiveValue(null);
+                    uploadMessageAboveL = null;
+                }
+                uploadMessageAboveL = filePathCallback;
+                Intent intent = fileChooserParams.createIntent();
+                try {
+                    startActivityForResult(intent, FILE_CHOOSER_RESULT_CODE);
+                } catch (ActivityNotFoundException e) {
+                    uploadMessageAboveL = null;
+                    Toast.makeText(getBaseContext(), "Cannot Open File Chooser", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                return true;
+            }
+
+            //For Android 4.1 only
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                uploadMessage = uploadMsg;
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image");
+                startActivityForResult(Intent.createChooser(intent, "File Browser"), FILE_CHOOSER_RESULT_CODE);
+            }
+
+            //for Android <3.0
+            protected void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                uploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILE_CHOOSER_RESULT_CODE);
+            }
+
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, true);
+                super.onGeolocationPermissionsShowPrompt(origin, callback);
+            }
+
+
+        });
     }
 
     private void wxLoginComplete(JSONObject jsonObject){
@@ -165,6 +238,29 @@ public class MainActivity extends AppCompatActivity implements JavaScriptInterfa
                 webView.loadUrl(jsStr);
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.ECLAIR_MR1)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+                if (uploadMessageAboveL == null)
+                    return;
+                uploadMessageAboveL.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, intent));
+                uploadMessageAboveL = null;
+            }
+        } else if (requestCode == FILE_CHOOSER_RESULT_CODE) {
+            if (null == uploadMessage)
+                return;
+            // Use MainActivity.RESULT_OK if you're implementing WebView inside Fragment
+            // Use RESULT_OK only if you're implementing WebView inside an Activity
+            Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
+            uploadMessage.onReceiveValue(result);
+            uploadMessage = null;
+        } else {
+            Toast.makeText(getBaseContext(), "Failed to Upload Image", Toast.LENGTH_LONG).show();
+        }
     }
 
 }
